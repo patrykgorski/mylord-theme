da734307343fec838b1407acd96fb3060817ccdc
Patryk Górski <kontakt@gorskipatryk.pl>
2015-12-02 22:53:17 +0100

    Create Home page

--- src/js/nav.js
+++ src/js/nav.js
@@ -10,38 +10,49 @@
 // 	}
 // }
 
-/*ALERT INFO*/
 
-$('.status-info')
-  .popup({
-    popup : $('.custom.popup'),
-    on    : 'click',
-    position : 'bottom center'
- });
+$(function(){
+	/*ALERT INFO*/
+	$('.status-info')
+	  .popup({
+	    popup : $('.custom.popup'),
+	    on    : 'click',
+	    position : 'bottom center'
+	 });
 
-$('.page-nav-button').on('click', function(){
-	if($(this).hasClass('alert-button')){
-		$('span.info-ico').hide();
-		$('.status-info')
-		  .popup({
-		    popup : $('.custom.popup'),
-		    on    : 'dblclick',
-		    position : 'bottom center'
-		 });
-	}
+	$('.page-nav-button').on('click', function(){
+		if($(this).hasClass('alert-button')){
+			$('span.info-ico').hide();
+			$('.status-info')
+			  .popup({
+			    popup : $('.custom.popup'),
+			    on    : 'dblclick',
+			    position : 'bottom center'
+			 });
+		}
 
-	if($(this).hasClass('active')){
+		if($(this).hasClass('active')){
 
-	}else{
-		$('.page-nav-button').removeClass('active');
-		$(this).addClass('active');
-		var lenli = $('.page-nav-button').length;
-		for (var i = 0; i <= lenli-1; i++) {
-			if($('.page-nav-button').eq(i).hasClass('active')){
-				$('.sub-menu').hide();
-				$('.sub-menu').eq(i).show();
+		}else{
+			$('.page-nav-button').removeClass('active');
+			$(this).addClass('active');
+			var lenli = $('.page-nav-button').length;
+			for (var i = 0; i <= lenli-1; i++) {
+				if($('.page-nav-button').eq(i).hasClass('active')){
+					$('.sub-menu').hide();
+					$('.sub-menu').eq(i).fadeIn();
+				}
 			}
 		}
-	}
+	})
+
+	/*Close popup*/
+    $('.close-btn .icon')
+      .popup({
+        position : 'left center'
+      })
+    ;
 })
+
+
 
